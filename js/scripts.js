b81d51ac1bd30d1cada67748adca48a0aa3a9b7f
Anton Sharykin and Nina Potrebich <nina@>
2019-10-30 16:31:57 -0700

    It wiil add regular expressions in scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -14,27 +14,18 @@ $(document).ready(function() {
 });
 
 var pigLatin = function(word){
-  var isNumber = isNaN(parseInt(word));
-  if (isNumber === false) {
-    return word
-  } else if(word[0] === "a" || word[0] === "e" || word[0] === "i" || word[0] === "o" || word[0] === "u" || word[0] === "A" | word[0] === "E" | word[0] === "I" | word[0] === "O" | word[0] === "U") {
-    return word + "way"
-  } else {
-    var count = 0;
-    for(var index = 1; index < word.length; index++){
-      if (word[index] === "u" && word[index-1] === "q"){
-        count = index+1;
-        break;
-      }
-      else if (word[index] === "a" || word[index] === "e" || word[index] === "i" || word[index] === "o" || word[index] === "u" || word[index] === "A" | word[index] === "E" | word[index] === "I" | word[index] === "O" | word[index] === "U" || word[index] === "y") {
-        count = index;
-        break;
-      }
-    }
-    return word.slice(count, count.length) + word.slice(0, count) + "ay"
+  if (/\d/.test(word)){
+    return(word);
+  } else if (/^[aeiou]/.test(word)){
+    return(word + "way")
+  } else if  (/^[^aeioqu]{0,}qu/.test(word)) {
+    var match = /^[^aeioqu]{0,}qu/.exec(word)[0];
+    return(word.slice(match.length) + match + "ay")
+  } else if ( /^[^aeiouy]{1,}/.test(word)){
+    var match = /^[^aeiouy]{1,}/.exec(word)[0];
+    console.log(match);
+    return(word.slice(match.length) + match + "ay")
+  } else if (/^y/.test(word)) {
+    return(word.slice(1,) + 'y' + "ay")
   }
-
-
-  //return(word)
-
 }
